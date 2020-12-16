fab9244b56092f2ead6bcbae5eb3d31ebc626397
Joshua Sung <yuniksung@gmail.com>
2020-12-16 17:39:05 -0600

    chapter 3 done

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ db.all(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
 });
 
-
+//*************************************
+// ******** Candidate routes **********
+//*************************************
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
   const sql = `SELECT candidates.*, parties.name 
@@ -115,6 +117,81 @@ app.post('/api/candidate', ({ body }, res) => {
       data: body,
       id: this.lastID
     });
+  });
+});
+
+app.put('/api/candidate/:id', (req, res) =>{
+  const errors = inputCheck(req.body, 'party_id');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `UPDATE candidates SET party_id = ? WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status (400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: req.body,
+      changes: this.changes
+    });
+  });
+});
+
+
+//*************************************
+// *********** Party routes ***********
+//*************************************
+// Get all parties
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Get single party
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Delete a party
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id]
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+
+    res.json({ message: 'successfully deleted', changes: this.changes });
   });
 });
 
