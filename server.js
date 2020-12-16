f51edab5e8da941225f1027b3c9833270a653d06
Joshua Sung <yuniksung@gmail.com>
2020-12-16 17:23:55 -0600

    3.5

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ db.all(`SELECT * FROM candidates`, (err, rows) => {
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+            AS party_name 
+            FROM candidates 
+            LEFT JOIN parties 
+            ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
       if (err) {
@@ -43,8 +47,12 @@ app.get('/api/candidates', (req, res) => {
 
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates 
-                 WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
       if (err) {
@@ -61,7 +69,12 @@ app.get('/api/candidate/:id', (req, res) => {
 
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+            AS party_name 
+            FROM candidates 
+            LEFT JOIN parties 
+            ON candidates.party_id = parties.id 
+            WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.run(sql, params, function(err, result) {
       if (err) {
@@ -84,8 +97,11 @@ app.post('/api/candidate', ({ body }, res) => {
     return;
   }
 
-  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-              VALUES (?, ?, ?)`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
   const params = [body.first_name, body.last_name, body.industry_connected];
   // ES5 function, no arrow function here
   db.run(sql, params, function(err, result) {
